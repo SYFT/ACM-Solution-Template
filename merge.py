d47ad8e21081e126a6e4f67c741e85fd74591432
stupidboy <smallrqnoj@163.com>
2016-09-24 21:05:41 +0800

    20160914 add merge.py

---
+++ merge.py
@@ -0,0 +1,21 @@
+import os
+
+fileDir = '.'
+resultName = 'template.txt'
+resultFile = os.path.join(fileDir, resultName)
+
+with open(resultFile, 'w') as result :
+	for fName in os.listdir(fileDir) :
+		if fName.find('.cpp') != len(fName) - 4 :
+			continue
+
+		result.write(fName + ':\n')
+		fName = os.path.join(fileDir, fName)
+		template = open(fName, 'r')
+		content = template.read()
+		template.close()
+		result.write(content)
+		result.write('\n\n\n')
+
+
+result.close()
